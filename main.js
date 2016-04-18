29603e9533224cc7677b4f6cdb9e51185ff32f1b
Lou Huang <lou@louhuang.com>
2016-04-18 11:10:36 -0400

    Bump Tangram 0.7; use import scene file; revert local bubble-wrap to canonical 1.1

--- main.js
+++ main.js
@@ -7,35 +7,9 @@ var hash = new L.Hash(map)
 
 // Add Tangram scene layer
 var layer = Tangram.leafletLayer({
-  scene: 'https://mapzen.com/carto/bubble-wrap-style/1.1/bubble-wrap.yaml',
-  // scene: 'scene/bubble-wrap.yaml',
+  scene: 'scene/sf-buildings-lidar-preview.yaml',
   attribution: 'Rendering by <a href="https://mapzen.com/tangram">Tangram</a> | &copy; OSM contributors'
 }).addTo(map)
-
-layer.scene.subscribe({
-  load: function (msg) {
-    var scene = msg.config
-
-    // Use demo-specific vector tiles API key
-    scene.sources.osm.url = 'https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt?api_key=' + VECTOR_TILES_API_KEY
-
-    scene.sources.sfbuildings = {
-      type: 'GeoJSON',
-      url: 'https://s3-us-west-2.amazonaws.com/openmassing/tiles/us/ca/san_francisco/{z}/{x}/{y}.json.gz',
-      min_zoom: 16,
-      max_zoom: 16
-    }
-
-    scene.layers.buildings.data.source = 'sfbuildings'
-    // This prevents sfbuildings from being filtered out
-    delete scene.layers.buildings.extrude.filter
-    // This removes the scaling effect that happens on zoom
-    delete scene.styles['scale-buildings']
-    // Don't fictionally extrude buildings
-    scene.layers.buildings.extrude.draw.polygons.extrude = function() { return feature.height || 0; }
-    scene.layers.buildings.extrude.draw.lines.extrude = function() { return feature.height || 0; }
-  }
-})
 
 var toggleEl = document.getElementById('toggle')
 var labelEl = document.getElementById('label')
